ee6bb52503c3e0c3e7c08fbfced55b4b6cfecc65
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-09-26 10:11:27 -0400

    Completed Shopping List

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1.milk 2.eggs 3.chicken 4.juice 5.cheese 6.strawberries 7.toothpaste 8.candy 9.bread 10.cereal");
+    }
+}
